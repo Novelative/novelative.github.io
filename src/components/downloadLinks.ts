2f318a8b19937685ac600b0e14a8b11fd5a8c6f4
Novelative <support@novelative.com>
2026-06-10 21:53:05 -0600

    Revert "added a bunch of download page buttons on the home page"
    This reverts commit 0a5f45a5276c7c2358bd54385541b3406aaa85b7.
    
    reverted the commit

--- src/components/downloadLinks.ts
+++ src/components/downloadLinks.ts
@@ -1,33 +1,4 @@
-import { Apple } from "lucide-react";
-import { createElement, type SVGProps } from "react";
-
-type PlatformIconProps = SVGProps<SVGSVGElement> & {
-  size?: number | string;
-};
-
-export function WindowsIcon({
-  size = 24,
-  width,
-  height,
-  ...props
-}: PlatformIconProps) {
-  return createElement(
-    "svg",
-    {
-      ...props,
-      width: width ?? size,
-      height: height ?? size,
-      viewBox: "0 0 448 512",
-      role: "img",
-      "aria-hidden": props["aria-hidden"] ?? true,
-      focusable: "false",
-      fill: "currentColor",
-    },
-    createElement("path", {
-      d: "M0 93.7l183.6-25.3 0 177.4-183.6 0 0-152.1zM0 418.3l183.6 25.3 0-175.2-183.6 0 0 149.9zm203.8 28l244.2 33.7 0-211.6-244.2 0 0 177.9zm0-380.6l0 180.1 244.2 0 0-213.8-244.2 33.7z",
-    }),
-  );
-}
+import { Apple, MonitorDown } from "lucide-react";
 
 export const winDownloadUrl =
   "https://github.com/Novelative/Win-User-Releases/releases/latest/download/Novelative-Setup.exe";
@@ -41,7 +12,7 @@ export const downloadBuilds = [
     file: "Novelative-Setup.exe",
     size: "108.9 MB",
     href: winDownloadUrl,
-    icon: WindowsIcon,
+    icon: MonitorDown,
   },
   {
     platform: "macOS Installer",
